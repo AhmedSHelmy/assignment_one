87324023a8db4455c7ae5551dc3dd0ba0df02f68
AhmedSHelmy <ahmed.s.helmy649@gmail.com>
2024-09-17 14:57:15 +0300

    assignment 1

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,134 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: const Text('First'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.all(10),
-              height: 100,
-              color: Colors.amberAccent,
-
-            ),
-            Container(
-              margin: EdgeInsets.all(10),
-              height: 100,
-              color: Colors.blue,
-            ),
+          backgroundColor: Colors.white,
+          leading: Icon(Icons.menu),
+          title: const Text('Assignmnet 1'),
+          actions: [
+            Icon(Icons.search)
           ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                    decoration: BoxDecoration(
+                        color: Colors.amberAccent,
+                        borderRadius: BorderRadius.all(Radius.circular(20))
+                    ),
+                    padding: EdgeInsets.all(15),
+                    margin: EdgeInsets.all(20),
+                    height: 150,
+                    width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Top Right', style: TextStyle(color: Colors.white ,fontSize: 20),),
+                    Center(child: Text('Center', style: TextStyle(color: Colors.white,fontSize: 20))),
+                    Align(alignment: Alignment.bottomRight,
+                    child: Text('Bottom Right', style: TextStyle(color: Colors.white,fontSize: 20))),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.all(20),
+                height: 150,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Top Right', style: TextStyle(color: Colors.white ,fontSize: 20),),
+                    Center(child: Text('Center', style: TextStyle(color: Colors.white,fontSize: 20))),
+                    Align(alignment: Alignment.bottomRight,
+                        child: Text('Bottom Right', style: TextStyle(color: Colors.white,fontSize: 20))),
+                  ],
+                ),
+              ),Container(
+                decoration: BoxDecoration(
+                    color: Colors.redAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.all(20),
+                height: 150,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Top Right', style: TextStyle(color: Colors.white ,fontSize: 20),),
+                    Center(child: Text('Center', style: TextStyle(color: Colors.white,fontSize: 20))),
+                    Align(alignment: Alignment.bottomRight,
+                        child: Text('Bottom Right', style: TextStyle(color: Colors.white,fontSize: 20))),
+                  ],
+                ),
+              ),Container(
+                decoration: BoxDecoration(
+                    color: Colors.greenAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.all(20),
+                height: 150,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Top Right', style: TextStyle(color: Colors.white ,fontSize: 20),),
+                    Center(child: Text('Center', style: TextStyle(color: Colors.white,fontSize: 20))),
+                    Align(alignment: Alignment.bottomRight,
+                        child: Text('Bottom Right', style: TextStyle(color: Colors.white,fontSize: 20))),
+                  ],
+                ),
+              ),Container(
+                decoration: BoxDecoration(
+                    color: Colors.orangeAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.all(20),
+                height: 150,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Top Right', style: TextStyle(color: Colors.white ,fontSize: 20),),
+                    Center(child: Text('Center', style: TextStyle(color: Colors.white,fontSize: 20))),
+                    Align(alignment: Alignment.bottomRight,
+                        child: Text('Bottom Right', style: TextStyle(color: Colors.white,fontSize: 20))),
+                  ],
+                ),
+              ),Container(
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.all(20),
+                height: 150,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Top Right', style: TextStyle(color: Colors.white ,fontSize: 20),),
+                    Center(child: Text('Center', style: TextStyle(color: Colors.white,fontSize: 20))),
+                    Align(alignment: Alignment.bottomRight,
+                        child: Text('Bottom Right', style: TextStyle(color: Colors.white,fontSize: 20))),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
